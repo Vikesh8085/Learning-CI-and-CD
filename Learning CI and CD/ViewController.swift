f9709cded9026f926918a8ce62d859610b62b8e3
Vikesh <Vikesh.prasad@mobiotics.com>
2020-03-18 13:01:50 +0530

    Initial Commit

---
+++ Learning CI and CD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learning CI and CD
+//
+//  Created by Vikesh Prasad on 18/03/20.
+//  Copyright © 2020 Mobiotics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Learning CI and CDTests/Learning_CI_and_CDTests.swift
@@ -0,0 +1,34 @@
+//
+//  Learning_CI_and_CDTests.swift
+//  Learning CI and CDTests
+//
+//  Created by Vikesh Prasad on 18/03/20.
+//  Copyright © 2020 Mobiotics. All rights reserved.
+//
+
+import XCTest
+@testable import Learning_CI_and_CD
+
+class Learning_CI_and_CDTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

a67063d56a220e2473da8e9aec7888341265b446
Vikesh <Vikesh.prasad@mobiotics.com>
2020-03-18 13:57:09 +0530

    testing Config

--- Learning CI and CDTests/Learning_CI_and_CDTests.swift
+++ Learning CI and CDTests/Learning_CI_and_CDTests.swift
@@ -20,8 +20,8 @@ class Learning_CI_and_CDTests: XCTestCase {
     }
 
     func testExample() {
-      let i = 100
-      XCTAssert(i == 100, "Demo Integer is not equal")
+      let i = 10
+      XCTAssert(i == 10, "Demo Integer is not equal")
     }
 
     func testPerformanceExample() {
